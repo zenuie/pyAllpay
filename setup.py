6ae0eee6b7990cb83186e47a60d47210f418257d
Jason2592 <formosa104@yahoo.com.tw>
2022-05-16 15:18:10 +0800

    修改 package 名稱, 將 Branch 拿掉

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ try:
 except:
     from distutils.core import setup
 
-__version__ = '0.0.12.dev1.1'
+__version__ = '0.0.12.dev2'
 __author__ = 'Calvin Jeng'
 __email__ = 'lock4567@gmail.com'
 
@@ -24,7 +24,7 @@ Check out github repo: https://github.com/lockys/allPay.py
 
 
 setup(
-    name='pyallpay-py3',
+    name='pyallpay',
     version=__version__,
     author=__author__,
     author_email=__email__,
